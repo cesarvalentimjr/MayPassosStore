1cd0928ae606c2fe2466b3e5182ea5180325caec
cesarvalentimjr <cesarvalentimjr@gmail.com>
2025-08-25 17:21:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -140,7 +140,16 @@ def load_image(image_path):
 
 # Header da aplicação com logo
 st.markdown('<div class="header-container">', unsafe_allow_html=True)
-st.image("logo.jpg", use_column_width=False, width=350)  # ajuste width se quiser maior/menor
+
+st.markdown(
+    """
+    <div style="display: flex; justify-content: center; align-items: center;">
+        <img src="logo.jpg" style="width: 400px;">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
 st.markdown('</div>', unsafe_allow_html=True)
 
 # Carregar produtos
